01eb170844f77d340944b7dce40b621c829fd885
Simon Schmid <mail@sschmid.com>
2013-07-05 16:57:58 +0200

    Initial Commit

---
+++ Floc-Reflection/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Floc-Reflection
+//
+//  Created by Simon Schmid on 05.07.13.
+//  Copyright (c) 2013 Simon Schmid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Floc-ReflectionTests/Floc_ReflectionTests.h
@@ -0,0 +1,13 @@
+//
+//  Floc_ReflectionTests.h
+//  Floc-ReflectionTests
+//
+//  Created by Simon Schmid on 05.07.13.
+//  Copyright (c) 2013 Simon Schmid. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Floc_ReflectionTests : SenTestCase
+
+@end
